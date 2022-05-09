2a157760037f9bc513431dbb5675ae24e5b1f04b
Hafiz <hafizalimi77@gmail.com>
2022-05-09 17:52:01 +0100

    completed plain text task

--- directory.rb
+++ directory.rb
@@ -1,33 +1,53 @@
-#let's put student into a array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Dark Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Micheal Corleno", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
-
+def input_students
+    puts "Please enter the names of the students"
+    puts "TO finsh, just hit return twice"
+    students = []
+    name = gets.chomp
+    puts "Please enter the cohort you'll like"
+    cohort = gets.chomp
+    if cohort.length.zero?
+      cohort << "Decmber"
+    end
+    #while the name is not empty, repeat this code
+    count = 1
+    while !name.empty? do
+      #add the students hash to the array
+      students << { name: name, cohort: cohort.to_sym }
+      while count <= students.length
+        if name.start_with?('a','b','j','p') && name.length < 12
+          students.map { |s| s[:cohort] }.uniq.each { |c| puts "#{c} cohort students are #{students.find_all { |s| s[:cohort] == c }.map { |s| s[:name] }.join(', ')}" }
+          count += 1
+          break
+        else
+          students.delete(name)
+        end
+      end
+      if students.count == 1
+        puts "Now we have #{students.count} student"
+      else
+        puts "Now we have #{students.count} students"
+      end
+      # puts "Now we have #{students.count} student"
+      #get snother name from user
+      puts "Please enter another name"
+      name = gets.chomp
+      puts "Please enter cohort"
+      cohort = gets.chomp
+    end
+    # return the array of the students
+    students
+end
+  
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
+  
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-
+  
+students = input_students
 print_header
 print(students)
 print_footer(students)
