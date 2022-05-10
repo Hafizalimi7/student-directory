cf7cfabdcf56e995f1b183cd74ca21b5d451ec15
Hafiz <hafizalimi77@gmail.com>
2022-05-10 14:39:04 +0100

    loaded data from file

--- directory.rb
+++ directory.rb
@@ -26,6 +26,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from the student.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -71,6 +72,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| 
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
